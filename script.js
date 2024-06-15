50cc143d5bd2fea9b4770191f275e344afabb2da
Pusp anand <146056018+anandclicks@users.noreply.github.com>
2024-06-15 18:36:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const fetchdata = async()=> {
+try {
+      const fetchdata = async()=> {
     const result = await fetch("http://localhost:3000/users")
      const data = await result.json()
      console.log(data)
 }
 fetchdata()
+} catch (error) {
+    
+}
